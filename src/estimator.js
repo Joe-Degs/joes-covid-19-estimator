38c5cad9bdf8639309c07e09225a8aed615f54f8
Joe-Degs <jpedegs8990@gmail.com>
2020-04-14 13:11:36 +0000

    Hohh

--- src/estimator.js
+++ src/estimator.js
@@ -1,11 +1,11 @@
 const duration = (periodType, timeToElapse) => {
-	const toDays = {
-		days: timeToElapse,
-		months: timeToElapse * 30,
-		weeks: timeToElapse * 7
-	}
-	return Math.trunc(toDays[periodType] / 3)
-}
+  const toDays = {
+    days: timeToElapse,
+    months: timeToElapse * 30,
+    weeks: timeToElapse * 7
+  };
+  return Math.trunc(toDays[periodType] / 3);
+};
 
 const percentage = (percent, number) => Math.trunc((percent / 100) * number);
 
@@ -24,10 +24,10 @@ const covid19ImpactEstimator = (data) => {
     estimate[key].infectionsByRequestedTime = estimate[key].currentlyInfected
      * (2 ** duration(data.periodType, data.timeToElapse));
 
-    severeCases = percentage(15, estimate[key].infectionsByRequestedTime);
-    requiredBeds = percentage(35, data.totalHospitalBeds);
+    const severeCases = percentage(15, estimate[key].infectionsByRequestedTime);
+    const requiredBeds = percentage(35, data.totalHospitalBeds);
     estimate[key].severeCasesByRequestedTime = severeCases;
-    estimate[key].hospitalBedsByRequestedTime = requiredBeds - severeCases; 
+    estimate[key].hospitalBedsByRequestedTime = requiredBeds - severeCases;
   });
 
   return estimate;
