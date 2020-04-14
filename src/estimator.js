1c1e9124d5bb21b2bb630c46b9ce258de396e50f
Joe-Degs <jpedegs8990@gmail.com>
2020-04-14 10:26:58 +0000

    chapter one linted

--- src/estimator.js
+++ src/estimator.js
@@ -1,35 +1,33 @@
+function duration(periodType, timeToElapse) {
+  if (periodType === 'months') {
+    const monthsToDays = timeToElapse * 30;
+    return Math.floor(monthsToDays / 3);
+  }
+  if (periodType === 'weeks') {
+    const weeksToDays = timeToElapse * 7;
+    return Math.floor(weeksToDays / 3);
+  }
+
+  return Math.floor(timeToElapse / 3);
+}
+
 const covid19ImpactEstimator = (data) => {
+  const estimate = {
+    data,
+    impact: {
+      currentlyInfected: data.reportedCases * 10
+    },
+    severeImpact: {
+      currentlyInfected: data.reportedCases * 50
+    }
+  };
 
-	const estimate = {
-		data: data,
-		impact: {
-			currentlyInfected: data.reportedCases * 10
-		},
-		severeImpact: {
-			currentlyInfected: data.reportedCases * 50
-		}
-	}
+  for (const key of Object.keys(estimate).slice(1, 3)) {
+    estimate[key].infectionsByRequestedTime = estimate[key].currentlyInfected * (2 ** duration(data.periodType, data.timeToElapse));
+  }
 
-	for (const key of Object.keys(estimate).slice(1,3)) {
-		estimate[key]['infectionsByRequestedTime'] =
-			estimate[key]['currentlyInfected'] * (2 ** duration(data.periodType, data.timeToElapse));
-	}
-
-	return estimate;
+  return estimate;
 };
 
-function duration (periodType, timeToElapse) {
-	if (periodType === 'months') {
-		monthsToDays = timeToElapse * 30;
-		return Math.floor(monthsToDays / 3);
-	}
-	else if (periodType === 'weeks') {
-		weeksToDays = timeToElapse * 7;
-		return Math.floor(weeksToDays / 3);
-	}
-	else {
-		return Math.floor(timeToElapse / 3);
-	}
-}
 
 export default covid19ImpactEstimator;
