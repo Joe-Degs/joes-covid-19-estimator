e974f547acd2e518e8f9ed050987b9598bbc98b6
Joe-Degs <jpedegs8990@gmail.com>
2020-04-14 21:27:14 +0000

    Ready to host

--- src/app.js
+++ src/app.js
@@ -2,40 +2,41 @@ const app = require('express')();
 const covid19 = require('./routes');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 8080;
 const fs = require('fs');
 const logFile = require('path').join(__dirname, 'logfile');
 
 morgan.token('response-time', () => {
-	let start = new Date();
-	let end = new Date() * 0.5;
-	let timeDiff = Math.round(start-end);
-	timeDiff = timeDiff.toString().padStart(2, 0);
-	// timeDiff = +timeDiff
+  const start = new Date();
+  const end = new Date() * 0.5;
+  let timeDiff = Math.round(start - end);
+  timeDiff = timeDiff.toString().padStart(2, 0);
+  // timeDiff = +timeDiff
 
-	return +timeDiff;
+  return +timeDiff;
 });
 
 app.use(bodyParser.json());
 
 app.use(morgan(':method\t\t:url\t\t:status\t\t0:total-time[0]ms', {
-	stream: fs.createWriteStream(logFile, {flags: 'a'})
+  stream: fs.createWriteStream(logFile, { flags: 'a' })
 }));
 
 app.use('/api/v1/on-covid-19/', covid19);
 
 app.use((req, res, next) => {
-	let err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-})
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 app.use((err, req, res) => {
-	res.status(error.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: err
-	})
-})
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
+});
 
 app.listen(PORT);
--- src/routes.js
+++ src/routes.js
@@ -5,33 +5,30 @@ const fs = require('fs');
 const logFile = require('path').join(__dirname, 'logfile');
 
 router.post('/', (req, res) => {
-	console.log(req.body);
-	const result = estimator(req.body);
-	return res.status(200).json(result);
+  const result = estimator(req.body);
+  return res.status(200).json(result);
 });
 
 router.post('/:resType', (req, res) => {
-	console.log(req.body);
-	const result = estimator(req.body);
+  const result = estimator(req.body);
 
-	if ( req.params.resType === 'xml' ) {
-		const options = {compact: true, ignoreComment: true, spaces: 4};
-		const XMLres = toXML(result, options);
-		res.setHeader('Content-Type', 'application/xml');
-		return res.status(200).send(XMLres);
-	}
+  if (req.params.resType === 'xml') {
+    const options = { compact: true, ignoreComment: true, spaces: 4 };
+    const XMLres = toXML(result, options);
+    res.setHeader('Content-Type', 'application/xml');
+    return res.status(200).send(XMLres);
+  }
 
-	return res.status(200).json(result);
+  return res.status(200).json(result);
 });
 
 router.get('/logs', (req, res, next) => {
-	// return res.status(200).send('Hello World');
-	fs.readFile(logFile, 'utf8', (err, content) => {
-		if ( err ) return next(err);
+  fs.readFile(logFile, 'utf8', (err, content) => {
+    if (err) return next(err);
 
-		res.setHeader('Content-Type', 'text/plain');
-		return res.status(200).send(content)
-	});
+    res.setHeader('Content-Type', 'text/plain');
+    return res.status(200).send(content);
+  });
 });
 
 
